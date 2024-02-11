6dbff8454bd4cde0413c2822912838cdff9df987
Nona Xafier <159223109+NonaDev41@users.noreply.github.com>
2024-02-11 13:08:09 +0700

    Add files via upload

---
+++ Nonton.py
@@ -0,0 +1,98 @@
+#_____ MODULE BOKEP _____#
+import os,sys,random,requests,time
+#_____ WARNA WARNI _____#
+A = '\x1b[1;97m';R = '\x1b[38;5;196m';Y = '\033[1;33m';G = '\x1b[38;5;46m';B = '\x1b[38;5;8m';G1 = '\x1b[38;5;48m';G2 = '\x1b[38;5;47m';G3 = '\x1b[38;5;48m';G4 = '\x1b[38;5;49m';G5 = '\x1b[38;5;50m';X = '\33[1;34m';X1 = '\x1b[38;5;14m';X2 = '\x1b[38;5;123m';X3 = '\x1b[38;5;122m';X4 = '\x1b[38;5;86m';X5 = '\x1b[38;5;121m';S = '\x1b[1;96m';M = '\x1b[38;5;205m'
+#_____ BERSIHKAN LAYAR TERMINAL _____#
+#_____ LOGO BOKEP _____#
+tema=f"""{G1}╔╗╔╔═╗╔╗╔╔╦╗╔═╗╔╗╔  ╔╗ ╔═╗╦╔═╔═╗╔═╗
+{X5}║║║║ ║║║║ ║ ║ ║║║║  ╠╩╗║ ║╠╩╗║╣ ╠═╝
+{S}╝╚╝╚═╝╝╚╝ ╩ ╚═╝╝╚╝  ╚═╝╚═╝╩ ╩╚═╝╩  
+{A}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+{G1}[{A}≈{G1}]{A} Author   : {R}NonaXafier
+{G1}[{A}≈{G1}]{A} Tools    : {X5}Nonton Bokep
+{G1}[{A}≈{G1}]{A} Facebook : {S}Nona Xafier
+{G1}[{A}≈{G1}]{A} Github   : {B}NonaDev41
+{A}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"""
+#_____ BERSIHKAN LAYAR TERMINAL _____#
+def clear():os.system('clear');print(tema)
+#_____ GARIS ATAU LINE _____#
+def linex():print(f'{A}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+#_____ YAH BERJALAN JALAN _____#
+def berjalan(nona_dev):
+	for e in nona_dev + "\n":sys.stdout.write(e);sys.stdout.flush();time.sleep(0.030)
+#_____ MENU SIMONTOK _____#
+def MenuBokep():
+	clear()
+	print(f'{G1}[{A}1{G1}]{A} BOKEP XTUBECINEMA')
+	print(f'{G1}[{A}2{G1}]{A} BOKEP XNXX')
+	print(f'{G1}[{A}3{G1}]{A} BOKEP SIMONTOK')
+	linex()
+	print(f'{A}[{S}>{A}] Pilih Menu {X}> {A}1/{S}2{A}/{R}3')
+	linex()
+	BokepMu = input(f'{A}[{X2}>{A}] Choose {G}> {B}')
+	linex()
+	if BokepMu in ['1','01']:BokepMobile()
+	elif BokepMu in ['2','02']:BokepXnxx()
+	elif BokepMu in ['3','03']:BokepSimontok()
+	else:print(f'{A}>{R}!{A}< {G2}Pilihan Tidak Ada DiMenu');time.sleep(2);MenuBokep()
+#_____ BOKEP MOBILE _____#
+def BokepMobile():
+	clear()
+	print(f'{A}[{S}>{A}] Ketik {X5}Mulai {A} Untuk Mulai Nonton')
+	linex()
+	BokepMobile1 = input(f'{A}[{S}>{A}] Ketik : {R}')
+	if BokepMobile1 in ['Mulai','mulai','Mulaii']:
+		berjalan(f'{A}({S}>{A}) Tunggu Sebentar, Beberapa Detik Lagi Anda Akan DiArahkan Ke Link Bokep')
+		time.sleep(2)
+		os.system("xdg-open https://melewati.com/xtubecinema")
+		MenuBokep()
+	elif BokepMobile1 ['']:
+		berjalan(f'{A}({R}!{A}) Harap Ketik Sesuai Perintah {R}!!')
+		berjalan(f'{A}({R}!{A}) Silahkan Untuk Memulai Kembali')
+		BokepMobile()
+	else:
+		berjalan(f'{A}({R}!{A}) Harap Ketik Sesuai Perintah {R}!!')
+		berjalan(f'{A}({R}!{A}) Harap Ketik Sesuai Perintah {R}!!')
+		berjalan(f'{A}({R}!{A}) Silahkan Untuk Memulai Kembali')
+		BokepMobile()
+#_____ BOKEP XNXX _____#
+def BokepXnxx():
+	clear()
+	print(f'{A}[{S}>{A}] Ketik {X5}Mulai {A} Untuk Mulai Nonton')
+	linex()
+	BokepXnxx = input(f'{A}[{S}>{A}] Ketik : {R}')
+	if BokepXnxx in ['Mulai','mulai','Mulaii']:
+		berjalan(f'{A}({S}>{A}) Tunggu Sebentar, Beberapa Detik Lagi Anda Akan DiArahkan Ke Link Bokep')
+		time.sleep(2)
+		os.system("xdg-open https://dbjjj.com/")
+		MenuBokep()
+	elif BokepXnxx ['']:
+		berjalan(f'{A}({R}!{A}) Harap Ketik Sesuai Perintah {R}!!')
+		berjalan(f'{A}({R}!{A}) Silahkan Untuk Memulai Kembali')
+		BokepXnxx()
+	else:
+		berjalan(f'{A}({R}!{A}) Harap Ketik Sesuai Perintah {R}!!')
+		berjalan(f'{A}({R}!{A}) Harap Ketik Sesuai Perintah {R}!!')
+		berjalan(f'{A}({R}!{A}) Silahkan Untuk Memulai Kembali')
+		BokepXnxx()
+#_____ BOKEP SIMONTOK _____#
+def BokepSimontok():
+	clear()
+	print(f'{A}[{S}>{A}] Ketik {X5}Mulai {A} Untuk Mulai Nonton')
+	linex()
+	BokepSimontok = input(f'{A}[{S}>{A}] Ketik : {R}')
+	if BokepSimontok in ['Mulai','mulai','Mulaii']:
+		berjalan(f'{A}({S}>{A}) Tunggu Sebentar, Beberapa Detik Lagi Anda Akan DiArahkan Ke Link Bokep')
+		time.sleep(2)
+		os.system("xdg-open https://185.63.253.200/")
+		MenuBokep()
+	elif BokepSimontok ['']:
+		berjalan(f'{A}({R}!{A}) Harap Ketik Sesuai Perintah {R}!!')
+		berjalan(f'{A}({R}!{A}) Silahkan Untuk Memulai Kembali')
+		BokepSimontok()
+	else:
+		berjalan(f'{A}({R}!{A}) Harap Ketik Sesuai Perintah {R}!!')
+		berjalan(f'{A}({R}!{A}) Harap Ketik Sesuai Perintah {R}!!')
+		berjalan(f'{A}({R}!{A}) Silahkan Untuk Memulai Kembali')
+		BokepSimontok()
+MenuBokep()
